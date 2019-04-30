dc46cb289e478beb2cd0296f92cafad1076a24a6
jomichaelis <42468538+jomichaelis@users.noreply.github.com>
2019-04-30 16:50:37 +0200

    added nice styled cards instead of list in murdergame

--- src/views/MurderGame.vue
+++ src/views/MurderGame.vue
@@ -1,46 +1,78 @@
 <template>
-  <div class="murdergame">
-    <h1 class="subheading grey--text">Mörderspiel</h1>
+<div class="murdergame">
 
-    <v-container class="my-5">
+  <v-container class="my-5">
 
-      <v-card flat v-for="murder in murders" :key="murder.name">
-        <v-layout row wrap :class="`pa-3 murder ${murder.name}`">
-          <v-flex xs6 sm6 md2>
-            <div class="caption grey--text">Name</div>
-            <div>{{ murder.name }}</div>
+    <v-btn @click="sortBy('date')">sort by date</v-btn>
+
+    <v-flex xs12>
+      <v-card color="grey darken-2" class="white--text" v-for="murder in murders" :key="murder.name">
+        <v-layout mt-2>
+          <v-flex my-2 xs3>
+            <v-img src="https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1" height="125px" contain></v-img>
           </v-flex>
-          <v-flex xs6 sm6 md2>
-            <div class="caption grey--text">Datum</div>
-            <div>{{ murder.date }}</div>
-          </v-flex>
-          <v-flex xs6 sm6 md6>
-            <div class="caption grey--text">Tathergang</div>
-            <div>{{ murder.how }}</div>
-          </v-flex>
-          <v-flex xs6 sm6 md2>
-            <div>
-              <v-chip small :class="`${murder.rate} white--text my-2 caption`">{{ murder.rate }}</v-chip>
-            </div>
+          <v-flex xs9>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">{{murder.name}} († {{murder.date}})</div>
+                <div>{{murder.how}}</div>
+              </div>
+            </v-card-title>
           </v-flex>
         </v-layout>
-        <v-divider></v-divider>
+        <v-divider dark></v-divider>
+        <v-card-actions class="pa-3">
+          Bewerte den Tod von {{murder.name}}
+          <v-spacer></v-spacer>
+          <v-icon>star_border</v-icon>
+          <v-icon>star_border</v-icon>
+          <v-icon>star_border</v-icon>
+          <v-icon>star_border</v-icon>
+          <v-icon>star_border</v-icon>
+        </v-card-actions>
       </v-card>
+    </v-flex>
 
-    </v-container>
+  </v-container>
 
-  </div>
+
+</div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      murders: [
-        { name: 'Johannes', date: '30.04.2019', how: 'bliblablub1', rate: 'five'},
-        { name: 'Katha', date: '26.04.2019', how: 'bliblablub2', rate: '1'},
-        { name: 'Boris', date: '12.01.2019', how: 'bliblablub3', rate: '4'},
-        { name: 'Liesl', date: '26.11.2018', how: 'bliblablub4', rate: '3'},
+      murders: [{
+          name: 'Johannes',
+          date: '30.04.2019',
+          how: 'Vom Messer erstochen',
+          rate: 'five'
+        },
+        {
+          name: 'Katha',
+          date: '26.04.2019',
+          how: 'Mit dem Wasserglas erschlagen',
+          rate: '1'
+        },
+        {
+          name: 'Boris',
+          date: '12.01.2019',
+          how: 'Jumping like a Gans',
+          rate: '4'
+        },
+        {
+          name: 'Liesl',
+          date: '26.11.2018',
+          how: 'In Sonnernmilch ertränkt',
+          rate: '3'
+        },
+        {
+          name: 'Tobi',
+          date: '16.01.2019',
+          how: 'Jumping like a Gans',
+          rate: '4'
+        },
       ]
     }
   }
@@ -48,9 +80,10 @@ export default {
 </script>
 
 <style>
-.murder{
+.murder {
   border-left: 4px solid black;
 }
+
 .v-chip.five {
   background: black;
 }
